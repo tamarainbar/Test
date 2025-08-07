24832c6641a09c6ab1fcf23bb80e2f02a7115fa1
Tamara Inbar <tamarainbar@gmail.com>
2025-08-07 15:35:39 -0400

    basic squares

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 80) {
+            HStack (spacing: 10) {
+                ForEach(0..<5) { index in
+                    VStack (spacing: 20){
+                        ForEach(0..<6) { index in
+                            Rectangle()
+                                .frame(width: 60, height: 60)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+                //Spacer()
+            }
+            
+            HStack (spacing: 10) {
+                ForEach(0..<5) { index in
+                    VStack (spacing: 20){
+                        ForEach(0..<2) { index in
+                            Rectangle()
+                                .frame(width: 60, height: 60)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+                //Spacer()
+            }
         }
         .padding()
     }
