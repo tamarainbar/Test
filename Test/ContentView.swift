8f9cf5e1933b5498991b8442660c460eed4c6e9e
Tamara Inbar <tamarainbar@gmail.com>
2025-08-11 09:31:05 -0400

    button layout

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -9,35 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 80) {
-            HStack (spacing: 10) {
-                ForEach(0..<5) { index in
-                    VStack (spacing: 20){
-                        ForEach(0..<6) { index in
-                            Rectangle()
-                                .frame(width: 60, height: 60)
-                                .foregroundStyle(.gray)
+            VStack(spacing: 60) {
+                Rectangle()
+                HStack (spacing: 10) {
+                    ForEach(0..<5) { index in
+                        VStack (spacing: 20){
+                            ForEach(0..<6) { index in
+                                Rectangle()
+                                    .frame(width: 60, height: 60)
+                                    .foregroundStyle(.gray)
+                            }
                         }
                     }
                 }
-                //Spacer()
-            }
-            
-            HStack (spacing: 10) {
-                ForEach(0..<5) { index in
-                    VStack (spacing: 20){
-                        ForEach(0..<2) { index in
-                            Rectangle()
-                                .frame(width: 60, height: 60)
-                                .foregroundStyle(.gray)
+                var length = 60.0
+                VStack (spacing: 10){
+                    HStack (spacing: 10) {
+                        ForEach(1..<6) { index in
+                            ForEach(1..<3) { index2 in
+                                Button {
+                                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                                } label: {
+                                    if index2 == 2 {
+                                        Text("\(index + 5)")
+                                    } else if index2 == 2 && index = 5{
+                                        Text("0")
+                                    } else {
+                                        Text("\(index)")
+                                    }
+                                }
+                                .frame(width: length, height: length)
+                                .font(.title)
+                                .background(Color.purple)
+                                .tint(Color.purple)
+                                .buttonStyle(.borderedProminent)
+                                .cornerRadius(10)
+                            }
                         }
                     }
                 }
-                //Spacer()
+                
+                
             }
+            Button("Enter") {
+                
+            }
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+            .padding()
         }
-        .padding()
-    }
 }
 
 #Preview {
